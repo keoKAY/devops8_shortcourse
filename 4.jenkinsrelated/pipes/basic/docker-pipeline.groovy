33258afe0001a5fe21054023468c20960946ed0f
KEO KAY <keokay888@gmail.com>
2024-10-25 12:28:00 +0000

    build docker image and p ush it ot regsitry

--- 4.jenkinsrelated/pipes/basic/docker-pipeline.groovy
+++ 4.jenkinsrelated/pipes/basic/docker-pipeline.groovy
@@ -1,18 +1,60 @@
 pipeline{
     agent any 
+
+    environment{
+       USERNAME="69966"
+       IMAGE_NAME="docker-pipe-reactjs"
+       TAG="v1.0.0"
+    }
     stages{
-    
-        stage("Stage One"){
-            steps{
-                script{
-                    docker.image('node:lts').inside{
-                    sh "node -v "
-                    sh "npm -v"
-                }
-                }
+       stage("Git Checkout"){
+         steps {
+            git "https://gitlab.com/lyvanna544/reactjs-devop8-template.git"
+         }
+       }
+
+       stage("Build Image"){
+        steps{
+            // Dockerfile , docker-compose.yaml
+            script{
+                // docker.image("node:lts").inside{
+                //   sh """
+                //    npm install  
+                //    npm run build 
+                //   """
+                // }
+                def dockerImage =  docker.build("${USERNAME}/${IMAGE_NAME}:${TAG}")  
                 
             }
         }
-        
-    }
+       }
+
+       stage("Push Image"){
+        steps{
+
+            script{
+                  // This step should not normally be used in your script. Consult the inline help for details.
+                withDockerRegistry(credentialsId: 'DOCKERHUB' ) {
+                    // some block
+                    sh """
+                     docker push ${USERNAME}/${IMAGE_NAME}:${TAG}
+                    """
+                }
+            }
+          
+        }
+       }
+       }    
+    
 }
+        // stage("Stage One"){
+        //     steps{
+        //         script{
+        //             docker.image('node:lts').inside{
+        //             sh "node -v "
+        //             sh "npm -v"
+        //         }
+        //         }
+                
+        //     }
+        // }

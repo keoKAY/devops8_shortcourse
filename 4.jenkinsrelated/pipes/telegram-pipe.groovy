f1caa013cff17153fedd460abb34d1a3b1e577e1
KEO KAY <keokay888@gmail.com>
2024-10-30 11:46:41 +0000

    telegram pipelnie

--- 4.jenkinsrelated/pipes/telegram-pipe.groovy
+++ 4.jenkinsrelated/pipes/telegram-pipe.groovy
@@ -16,11 +16,13 @@ pipeline {
     post{
         always{
             script{
+                // special  character like !  . - , must  use \\ to escapeit
                 // def  message = "Error deployingthewebsite\n Have  anice weekend!"
                 def message ="""
-                Good Morningfrom Jenkins  
-                Congratulation !! 
-                Access the website: https://new-reactjs.devnerd.store
+                🚀 *Jenkins Build Completed*\n
+                *Build Number*: ${env.BUILD_NUMBER}\n
+                *Status*: ✅ SUCCESS\n
+    
                 """
                 sendTelegramMessage(message)
             }
@@ -30,7 +32,7 @@ pipeline {
 
 def sendTelegramMessage(message){
     sh """
-            curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID}  -d text="${message}"
+            curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage -d chat_id=${TELEGRAM_CHAT_ID}  -d text="${message}" -d parse_mode=MarkdownV2
             
             """
 }
